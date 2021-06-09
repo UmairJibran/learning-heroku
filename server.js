7c2c007af5c39fe618f28a1992e74af6d21394ca
Umair Jibran <umairjibran7@gmail.com>
2021-06-09 11:54:34 +0500

    set `PORT`

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const Express = require("express");
 
 const app = Express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use("/", (req, res) => {
 	res.status(200).json(req.url);
